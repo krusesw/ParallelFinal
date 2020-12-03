f405e1d21b5ca5392fecb21d62d29107a2caa8a8
Krusesw <krusesw@mail.uc.edu>
2020-12-02 21:09:19 -0500

    update

--- kernel.cu
+++ kernel.cu
@@ -1,7 +1,21 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <curand.h>
+#include <curand_kernel.h>
 #include "NumCpp.hpp"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/generate.h>
+#include <thrust/sort.h>
+#include <thrust/copy.h>
+#include <math.h> 
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <thrust/extrema.h>
+#include <thrust/execution_policy.h>
 
 #include <stdio.h>
 
@@ -13,41 +27,41 @@ __global__ void addKernel(int* c, const int* a, const int* b)
     c[i] = a[i] + b[i];
 }
 
-nc::NdArray<double> generateDataTestingForCLass(int datapoints, int constraintX, int constraintY, bool garunteedGoodClustering, int numclusters = 3) {
-    nc::NdArray<double> generatedDataset = nc::empty<double>(nc::Shape(1, 2));
+nc::NdArray<float> generateDataTestingForCLass(int datapoints, int constraintX, int constraintY, bool garunteedGoodClustering, int numclusters = 3) {
+    nc::NdArray<float> generatedDataset = nc::empty<float>(nc::Shape(1, 2));
     for (int i = 0; i < datapoints; i++) {
         if (garunteedGoodClustering) {
-            //       nc::NdArray<double> randomPointX = nc::random::uniform<double>(nc::Shape(1), 0, constraintX);
-            //       nc::NdArray<double> randomPointY = nc::random::uniform<double>(nc::Shape(1), 0, constraintY);
-            //       nc::NdArray<double> randomPoint = nc::append<double>(randomPointX, randomPointY, nc::Axis::NONE);
+            //       nc::NdArray<float> randomPointX = nc::random::uniform<float>(nc::Shape(1), 0, constraintX);
+            //       nc::NdArray<float> randomPointY = nc::random::uniform<float>(nc::Shape(1), 0, constraintY);
+            //       nc::NdArray<float> randomPoint = nc::append<float>(randomPointX, randomPointY, nc::Axis::NONE);
             //       nc::take()
             //       if
-            //       generatedDataset = nc::append<double>(generatedDataset, randomPoint, nc::Axis::ROW);
+            //       generatedDataset = nc::append<float>(generatedDataset, randomPoint, nc::Axis::ROW);
             //       nc::norm();
         }
         else {
-            nc::NdArray<double> randomPointX = nc::random::uniform<double>(nc::Shape(1), 0, constraintX);
-            nc::NdArray<double> randomPointY = nc::random::uniform<double>(nc::Shape(1), 0, constraintY);
-            nc::NdArray<double> randomPoint = nc::append<double>(randomPointX, randomPointY, nc::Axis::NONE);
-            generatedDataset = nc::append<double>(generatedDataset, randomPoint, nc::Axis::ROW);
+            nc::NdArray<float> randomPointX = nc::random::uniform<float>(nc::Shape(1), (float)0.0, (float)constraintX);
+            nc::NdArray<float> randomPointY = nc::random::uniform<float>(nc::Shape(1), (float)0.0, (float)constraintY);
+            nc::NdArray<float> randomPoint = nc::append<float>(randomPointX, randomPointY, nc::Axis::NONE);
+            generatedDataset = nc::append<float>(generatedDataset, randomPoint, nc::Axis::ROW);
         }
     }
     generatedDataset = nc::deleteIndices(generatedDataset, 0, nc::Axis::ROW);
     return generatedDataset;
 }
 
-nc::NdArray<double> euclidianDistanceMatrix(nc::NdArray<double> dataset) {
-    nc::NdArray<double> xPoints = nc::deleteIndices(dataset, 1, nc::Axis::COL);
-    nc::NdArray<double> yPoints = nc::deleteIndices(dataset, 0, nc::Axis::COL);
+nc::NdArray<float> euclidianDistanceMatrix(nc::NdArray<float> dataset) {
+    nc::NdArray<float> xPoints = nc::deleteIndices(dataset, 1, nc::Axis::COL);
+    nc::NdArray<float> yPoints = nc::deleteIndices(dataset, 0, nc::Axis::COL);
 
-    std::pair<nc::NdArray<double>, nc::NdArray<double>> meshpairX = nc::meshgrid(xPoints, xPoints);
-    std::pair<nc::NdArray<double>, nc::NdArray<double>> meshpairY = nc::meshgrid(yPoints, yPoints);
+    std::pair<nc::NdArray<float>, nc::NdArray<float>> meshpairX = nc::meshgrid(xPoints, xPoints);
+    std::pair<nc::NdArray<float>, nc::NdArray<float>> meshpairY = nc::meshgrid(yPoints, yPoints);
 
-    nc::NdArray<double> xDistances = nc::abs(std::get<0>(meshpairX) - std::get<1>(meshpairX));
-    nc::NdArray<double> yDistances = nc::abs(std::get<0>(meshpairY) - std::get<1>(meshpairY));
+    nc::NdArray<float> xDistances = nc::abs(std::get<0>(meshpairX) - std::get<1>(meshpairX));
+    nc::NdArray<float> yDistances = nc::abs(std::get<0>(meshpairY) - std::get<1>(meshpairY));
 
-    nc::NdArray<double> euclidianDistances = nc::sqrt(nc::power(xDistances, 2) + nc::power(yDistances, 2));
-    euclidianDistances = nc::replace(euclidianDistances, 0.0, (-1.0));
+    nc::NdArray<float> euclidianDistances = nc::sqrt(nc::power(xDistances, 2) + nc::power(yDistances, 2));
+    euclidianDistances = nc::replace(euclidianDistances, (float)0.0, (float)-1.0);
 
     return euclidianDistances;
 }
@@ -61,10 +75,11 @@ nc::NdArray<int> initialClusterAssignment(int datapoints, bool garunteedGoodClus
     return clusterAssignment;
 }
 
-void agglomerativeShortestLink(int datapoints, int numClusters, nc::NdArray<double> distances, nc::NdArray<int> clusterAssignments) {
-    nc::NdArray<double> distanceAssessment = nc::where(distances > 0.0, distances, 99999999999999999999.9);
-    nc::NdArray<double> min = nc::min(distanceAssessment);
-    double minValue = min(0, 0);
+
+void agglomerativeShortestLink(int datapoints, int numClusters, nc::NdArray<float> distances, nc::NdArray<int> clusterAssignments) {
+    nc::NdArray<float> distanceAssessment = nc::where(distances > (float) 0.0, distances, (float) 999999.9);
+    nc::NdArray<float> min = nc::min(distanceAssessment);
+    float minValue = min(0, 0);
     nc::NdArray<nc::uint32> minIndicies = nc::argmin(distanceAssessment, nc::Axis::NONE);
     int minInt = int(minIndicies(0, 0));
 
@@ -82,37 +97,41 @@ void agglomerativeShortestLink(int datapoints, int numClusters, nc::NdArray<doub
         rewrite = row;
     }
 
-    nc::NdArray<double> firstMergePointDistances = distances(distances.rSlice(), removal);
-    nc::NdArray<double> secondMergePointDistances = distances(distances.rSlice(), rewrite);
-    nc::NdArray<double> mergeSet = nc::stack({ firstMergePointDistances, secondMergePointDistances }, nc::Axis::COL);
+    nc::NdArray<float> firstMergePointDistances = distances(distances.rSlice(), removal);
+    nc::NdArray<float> secondMergePointDistances = distances(distances.rSlice(), rewrite);
+    nc::NdArray<float> mergeSet = nc::stack({ firstMergePointDistances, secondMergePointDistances }, nc::Axis::COL);
     mergeSet = nc::amin(mergeSet, nc::Axis::COL);
-    nc::NdArray<double> mergeSetRow = nc::deleteIndices(mergeSet, removal, nc::Axis::COL);
+    nc::NdArray<float> mergeSetRow = nc::deleteIndices(mergeSet, removal, nc::Axis::COL);
     mergeSetRow = nc::deleteIndices(mergeSetRow, rewrite, nc::Axis::COL);
-    nc::NdArray<double> negitiveOne = { -1.0 };
-    mergeSetRow = nc::append<double>(negitiveOne, mergeSetRow, nc::Axis::NONE);
-    nc::NdArray<double> mergeSetCol = nc::deleteIndices(mergeSetRow, 0, nc::Axis::COL);
+    nc::NdArray<float> negitiveOne = { -1.0 };
+    mergeSetRow = nc::append<float>(negitiveOne, mergeSetRow, nc::Axis::NONE);
+    nc::NdArray<float> mergeSetCol = nc::deleteIndices(mergeSetRow, 0, nc::Axis::COL);
 
     int clustersOG = clusterAssignments.shape().cols;
     nc::NdArray<int> clusterZeros = nc::zeros<int>(1, clustersOG);
     clusterZeros = nc::where(clusterZeros == 0, -1, -1);
     nc::NdArray<int> mergeInClusterOne = clusterAssignments.row(removal);
     for (int value : mergeInClusterOne) {
+        printf("%i", value);
         if (value > -1) {
             nc::NdArray<int> valueint = { value };
+            clusterZeros = nc::deleteIndices(clusterZeros, clustersOG - 1, nc::Axis::COL);
             clusterZeros = nc::append<int>(valueint, clusterZeros, nc::Axis::COL);
         }
     }
-    nc::NdArray<int> mergeInClusterTwo = clusterAssignments.row(removal);
+    nc::NdArray<int> mergeInClusterTwo = clusterAssignments.row(rewrite);
     for (int value : mergeInClusterTwo) {
+        printf("%i", value);
         if (value > -1) {
             nc::NdArray<int> valueint = { value };
+            clusterZeros = nc::deleteIndices(clusterZeros, clustersOG - 1, nc::Axis::COL);
             clusterZeros = nc::append<int>(valueint, clusterZeros, nc::Axis::COL);
         }
     }
 
     clusterAssignments = nc::deleteIndices(clusterAssignments, removal, nc::Axis::ROW);
     clusterAssignments = nc::deleteIndices(clusterAssignments, rewrite, nc::Axis::ROW);
-    clusterAssignments = nc::append<int>(clusterZeros(nc::Slice(0, 0), nc::Slice(0, clustersOG)), clusterAssignments, nc::Axis::ROW);
+    clusterAssignments = nc::append<int>(clusterZeros, clusterAssignments, nc::Axis::ROW);
 
     distances = nc::deleteIndices(distances, removal, nc::Axis::ROW);
     distances = nc::deleteIndices(distances, removal, nc::Axis::COL);
@@ -122,6 +141,10 @@ void agglomerativeShortestLink(int datapoints, int numClusters, nc::NdArray<doub
     distances = nc::stack({ mergeSetCol.reshape(datapoints - 2,1), distances }, nc::Axis::COL);
     distances = nc::stack({ mergeSetRow, distances }, nc::Axis::ROW);
 
+    printf("%s", "\n");
+    printf("%s", "\n");
+    printf("%s", "\n");
+    printf("%s", "\n");
     printf("%s", "\n");
     printf("%s", "\n");
     printf("%s", "\n");
@@ -138,125 +161,124 @@ void agglomerativeShortestLink(int datapoints, int numClusters, nc::NdArray<doub
     }
 }
 
+__global__ void agglomerativeShortestLinkCUDA(int datapoints, int numClusters, float* distances, int* clusterAssignments) {
+
+}
+
+struct gtz {
+    __host__ __device__ bool operator() (double x) { return x > 0.; }
+};
+
+struct gtzVec {
+    __host__ __device__ bool operator() (float x, float y) { return x > 0.; }
+};
+
+void sequentialRun() {
+    nc::NdArray<float> dataSet = generateDataTestingForCLass(10, 100, 100, false);
+    nc::NdArray<float> euclidianDistances = euclidianDistanceMatrix(dataSet);
+    nc::NdArray<int> clusterAssignments = initialClusterAssignment(10, false);
+
+    std::vector<float> distanceVector = euclidianDistances.toStlVector();
+    std::vector<int> clusterVector = clusterAssignments.toStlVector();
+
+    float* distancePointer = distanceVector.data();
+    int* clusterPointer = clusterVector.data();
+
+    //Calling this appearently makes cuda start faster for loading thrust
+    cudaFree(0);
+
+    //Convert Distance Vector to Thrust Vector for parallel compuation
+    //https://github.com/NVIDIA/thrust/
+    //WARNING: ACTIVELY BUGGED IN NEWEST VERSION OF CUDA
+    //IF YOU HAVE CUDA 11.0 OR 11.1, THIS WILL NOT WORK
+    //FOLLOW WORKAROUND HERE: 
+    printf("%s", "cuda starting");
+    thrust::device_vector<float> d_vec = distanceVector;
+    //Sort distance vector, reminder that -1 values will be at front
+
+    float *minElement = thrust::min_element(thrust::device, d_vec.begin(), d_vec.begin()+100, );
+
+    for (int i = 0; i < d_vec.size(); i++)
+        std::cout << "D[" << i << "] = " << d_vec[i] << std::endl;
+
+    //Find minimum non negative value
+    thrust::device_vector<float>::iterator iter1 = d_vec.begin();
+    thrust::device_vector<float>::iterator iter2 = thrust::find_if(d_vec.begin(), d_vec.begin() + 100, gtz());
+    int d = thrust::distance(iter1, iter2);
+
+    printf("%i", d);
+
+    int row = d / 10;
+    int col = d % 10;
+    int removal = 0;
+    int rewrite = 0;
+    if (row >= col) {
+        removal = row;
+        rewrite = col;
+    }
+    else {
+        removal = col;
+        rewrite = row;
+    }
+
+    printf("%i", row);
+    printf("%i", col);
+
+
+     //  thrust::host_vector<int> h_vec(32 << 20);
+     //  std::generate(h_vec.begin(), h_vec.end(), rand);
+    //   thrust::device_vector<int> d_vec = h_vec;
+    //   thrust::sort(d_vec.begin(), d_vec.end());
+    //   thrust::copy(d_vec.begin(), d_vec.end(), h_vec.begin());
+
+       //    float* cudaDistancePointer;
+       //    int* cudaClusterPointer;
+
+       //    int numbytes = 10 * 10 * sizeof(float);
+
+       //    cudaMalloc(&cudaDistancePointer, numbytes);
+       //    cudaMalloc(&cudaClusterPointer, numbytes);
+       //    cudaMemcpy(cudaDistancePointer, distancePointer, numbytes, cudaMemcpyHostToDevice);
+       //    cudaMemcpy(cudaClusterPointer, clusterPointer, numbytes, cudaMemcpyHostToDevice);
+
+       //    agglomerativeShortestLinkCUDA << <1, 10 >> > (10, 3, cudaDistancePointer, cudaClusterPointer);
+           //agglomerativeShortestLinkCUDA << <1, datapoints >> > (datapoints, numClusters, ffd.data(), ffd.data());
+
+  //  euclidianDistances.print();
+   // agglomerativeShortestLink(10, 3, euclidianDistances, clusterAssignments);
+
+}
+
+
+
 
 
 
 
 int main()
 {
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
+    bool exitLoop = false;
+    while (!exitLoop) {
+        std::cout << "Enter 1 to run sequential, Enter 2 to run parallel, Other key to exit: ";
+        int option = -1;
+        std::cin >> option;
+        if (std::cin.good()) {
+            if (option == 1) {
+                sequentialRun();
+            }
+            else if (option == 2) {
+                // cudaRun();
+            }
+            else {
+                return 0;
+            }
+        }
+        else {
+            return 0;
+        }
 
-    nc::NdArray<double> dataSet = generateDataTestingForCLass(10, 100, 100, false);
-    nc::NdArray<double> euclidianDistances = euclidianDistanceMatrix(dataSet);
-    nc::NdArray<int> clusterAssignments = initialClusterAssignment(10, false);
-    //   if (garunteedGoodClustering) {
-    //       datapoints = datapoints + numclusters;
-    //   }
-    euclidianDistances.print();
-    agglomerativeShortestLink(10, 3, euclidianDistances, clusterAssignments);
-
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
     }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
+    cudaDeviceSynchronize();
 
     return 0;
-}
-
-
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int* c, const int* a, const int* b, unsigned int size)
-{
-    int* dev_a = 0;
-    int* dev_b = 0;
-    int* dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel << <1, size >> > (dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-
-    return cudaStatus;
 }
